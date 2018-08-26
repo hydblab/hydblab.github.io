ecf39a26bfdf94cd05694d4f8f56cdd6cedd56d9
Alghost <alghost@moreum.co.kr>
2018-08-26 18:16:12 +0900

    검색 추가 완료

--- assets/search.js
+++ assets/search.js
@@ -7,8 +7,8 @@
 
             for (var i = 0; i < results.length; i++) {  // Iterate over the results
                 var item = store[results[i].ref];
-                appendString += '<li><a href="' + item.url + '"><h6>' + item.title + '</h6></a>';
-                appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+                appendString += '<div><a href="' + item.url + '" class="catalogue-item"><h1 class="catalogue-title">' + (i+1) + '. ' + item.title + '</h1></a>';
+                appendString += '<p>' + item.content.substring(0, 150) + '...</p></div>';
             }
 
             searchResults.innerHTML = appendString;
@@ -31,43 +31,37 @@
     }
 
     function trimmerEnKo(token) {
-        return token
+        token.str = token.str
             .replace(/^[^\w가-힣]+/, '')
             .replace(/[^\w가-힣]+$/, '');
+        return token
     };
 
     var searchTerm = getQueryVariable('query');
 
     if (searchTerm) {
-        document.getElementById('search-box').setAttribute("value", searchTerm);
-
         // Initalize lunr with the fields it will be searching on. I've given title
         // a boost of 10 to indicate matches on this field are more important.
-        var idx = lunr(function () {
+        let idx = lunr(function () {
             this.pipeline.reset();
             this.pipeline.add(
-                trimmerEnKo,
-                lunr.stopWordFilter,
-                lunr.stemmer
+               trimmerEnKo,
+               lunr.stopWordFilter,
+               lunr.stemmer
             );
-            this.field('id');
+            // this.use(trimmerEnKo);
+            this.ref('id');
             this.field('title', { boost: 10 });
             this.field('author');
             this.field('category');
             this.field('content');
+            this.field('id');
+            Object.keys(window.store).forEach(function (k) {
+                this.add(window.store[k])
+            }, this)
         });
 
-        for (var key in window.store) { // Add the data to lunr
-            idx.add({
-                'id': key,
-                'title': window.store[key].title,
-                'author': window.store[key].author,
-                'category': window.store[key].category,
-                'content': window.store[key].content
-            });
-
-            var results = idx.search(searchTerm); // Get lunr to perform a search
-            displaySearchResults(results, window.store); // We'll write this in the next section
-        }
+        var results = idx.search(searchTerm); // Get lunr to perform a search
+        displaySearchResults(results, window.store); // We'll write this in the next section
     }
 })();
